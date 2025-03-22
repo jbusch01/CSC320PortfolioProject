5d5dad49e518363338b4d41aa01b17316a480434
jbusch01 <jonrbusch@gmail.com>
2025-03-22 16:33:11 -0600

    Updated HomeManager class with structure

--- src/HomeManager.java
+++ src/HomeManager.java
@@ -2,20 +2,39 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class HomeManager {
-    Scanner scnr = new Scanner(System.in);
-
+    private Scanner scnr = new Scanner(System.in);
     ArrayList<Home> homeInventory = new ArrayList<>();
 
-    // Variables?
-
+    // Method for adding a home
     public void addHome() {
-        // Method for adding a home
+        // Prompt user for all home details
+        // Create new Home object
+        // Add it to homList
+
     }
+    // Method for listing the homes
     public void listHome() {
-        // Method for listing the homes
+        // Loop through homeList and display each home
+
     }
-    public boolean deleteHome() {
-        // Method for removing a home
+
+    // Method for removing a home
+    public boolean deleteHome(String homeID) {
+        return true;
+        
+        // Search for home by ID
+        // If found, remove from list and return true
+        // If not found, return false
+
+    }
+
+    // Method for updating a home
+    public boolean updateHome(String homeID) {
+        return true;
+        // Search for home by ID
+        // If found, prompt for new values and update fields (optional)
+        // Return true/false depending on success
+
     }
 
 }
